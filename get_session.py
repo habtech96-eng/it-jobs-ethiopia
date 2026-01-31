3644954154a54feb77879fc5e1638eb7e75ded95
habtech96-eng <habtech96@gmail.com>
2026-01-31 21:30:30 +0300

    Update session code output URL

--- get_session.py
+++ get_session.py
@@ -6,5 +6,5 @@ API_ID = 31987639
 API_HASH = '1de0b58ba938bca8cd47d2292d6e5669'
 
 with TelegramClient(StringSession(), API_ID, API_HASH) as client:
-    print("\nየአንተ STRING SESSION ኮድ ይህ ነው (በጥንቃቄ ቅዳው):\n")
+    print("https://telethon-session-builder--habtiet96.replit.app")
     print(client.session.save())
